fdfcb21e8eef5cc6000498aea81a9c92ce4cbe58
yurilrodrigues <rodriguesyuri769@gmail.com>
2023-09-13 16:02:17 -0300

    feat: Criando as rotas de ver extratos, transações, saldo e as rotas de inserir depositos e gastos na conta

--- src/controller/FinanceController.ts
+++ src/controller/FinanceController.ts
@@ -1,11 +1,11 @@
-import { Response, Request } from "express";
+import { Response } from "express";
 import { format } from "date-fns";
 import { prisma } from "../database/prismaClient";
 import { UserRequest } from "../types/Auth";
 
 export class FinanceController {
-  async newBuy(req: UserRequest, res: Response) {
-    const { payment, store, value } = req.body;
+  async newExpense(req: UserRequest, res: Response) {
+    const { payment, store, value, transation, product } = req.body;
     const date = format(new Date(), "dd/MM/yyyy");
     const hours = format(new Date(), "HH:mm:ss");
     const [hour, minutes] = hours.split(":");
@@ -13,7 +13,117 @@ export class FinanceController {
     const userId = req.userId;
 
     try {
-      const finance = await prisma.finance.create({
+      const userBalance = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+        select: {
+          balance: true,
+          account: true,
+          email: true,
+          created_At: true,
+          name: true,
+        },
+      });
+
+      if (userBalance && userBalance?.balance < value) {
+        return res.status(400).json({ error: "Insufficient balance" });
+      }
+
+      const user = await prisma.finance.create({
+        data: {
+          User: {
+            connect: {
+              id: userId,
+            },
+          },
+          FinanceData: {
+            create: {
+              product,
+              day,
+              hour,
+              minutes,
+              month,
+              payment,
+              store,
+              value,
+              year,
+              transation,
+            },
+          },
+        },
+        include: {
+          FinanceData: {
+            select: {
+              product: true,
+              day: true,
+              month: true,
+              year: true,
+              hour: true,
+              minutes: true,
+              payment: true,
+              store: true,
+              value: true,
+              transation: true,
+            },
+          },
+          User: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      });
+
+      const findUserBalance = await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          balance: {
+            decrement: value,
+          },
+        },
+        select: {
+          balance: true,
+        },
+      });
+
+      return res.json({
+        message: `Nova saída adicionada, seu saldo atual é de R$ ${findUserBalance?.balance}`,
+        user,
+      });
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  }
+  async newDeposit(req: UserRequest, res: Response) {
+    const { value, transation } = req.body;
+    const date = format(new Date(), "dd/MM/yyyy");
+    const hours = format(new Date(), "HH:mm:ss");
+    const [hour, minutes] = hours.split(":");
+    const [day, month, year] = date.split("/");
+    const userId = req.userId;
+
+    try {
+      const userBalance = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+        select: {
+          balance: true,
+          account: true,
+          email: true,
+          created_At: true,
+          name: true,
+        },
+      });
+
+      if (!userBalance) {
+        res.status(400).json({ error: "User and balance not found" });
+      }
+
+      const userFinance = await prisma.finance.create({
         data: {
           User: {
             connect: {
@@ -26,10 +136,9 @@ export class FinanceController {
               hour,
               minutes,
               month,
-              payment,
-              store,
               value,
               year,
+              transation,
             },
           },
         },
@@ -41,54 +150,161 @@ export class FinanceController {
               year: true,
               hour: true,
               minutes: true,
-              payment: true,
-              store: true,
               value: true,
+              transation: true,
+              id: true,
             },
           },
           User: {
             select: {
-              name:true
-            }
-          }
+              name: true,
+            },
+          },
         },
       });
-      res.status(201).json(finance);
+
+      if (!userFinance) {
+        res.status(400).json({ error: "Finance not created" });
+      }
+
+      const findUserBalance = await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          balance: {
+            increment: value,
+          },
+        },
+        select: {
+          balance: true,
+          Finance: {
+            select: {
+              FinanceData: {
+                where: {
+                  id: userFinance.FinanceData?.id,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      const financeUpdated = await prisma.finance.findFirst({
+        where: {
+          FinanceData: {
+            day,
+            month,
+            year,
+            hour,
+            minutes,
+            value,
+            transation,
+          },
+        },
+        include: {
+          FinanceData: {
+            select: {
+              day: true,
+              month: true,
+              year: true,
+              hour: true,
+              minutes: true,
+              transation: true,
+            }
+          }
+        }
+      });
+
+      return res.json({
+        message: `Nova entrada adicionada, seu saldo atual é de R$ ${findUserBalance?.balance}`,
+        financeUpdated,
+      });
+
     } catch (error) {
       res.status(400).json(error);
     }
   }
-
-  async getAll(req: UserRequest, res: Response) {
+  async getBalance(req: UserRequest, res: Response) {
     const userId = req.userId;
 
     try {
-      const finances = await prisma.finance.findMany({
+      const allBalancesByUser = await prisma.user.findFirst({
         where: {
-          userId,
+          id: userId,
+        },
+        select: {
+          balance: true,
+        },
+      });
+
+      if (!allBalancesByUser) {
+        res.status(400).json({ error: "User not found" });
+      }
+
+      res.status(200).json(allBalancesByUser);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  }
+  async getExtracts(req: UserRequest, res: Response) {
+    const userId = req.userId;
+    try {
+      const extracts = await prisma.finance.findMany({
+        where: {
+          User: {
+            id: userId,
+          },
         },
         select: {
           FinanceData: {
             select: {
-              year: true,
-              month: true,
+              transation: true,
               day: true,
               hour: true,
               minutes: true,
-              payment: true,
-              id: true,
-              store: true,
+              month: true,
+              year: true,
               value: true,
+              id: true,
             },
           },
-          User: {
-            select: {
-              name:true
-            }
-          }
         },
       });
-      res.status(200).json(finances);
+      res.status(200).json(extracts);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  }
+  async getTransations(req: UserRequest, res: Response) {
+    const userId = req.userId;
+    try {
+      const { transation } = req.body;
+      const allDeposits = await prisma.finance.findMany({
+        where: {
+          User: {
+            id: userId,
+          },
+          FinanceData: {
+            transation,
+          },
+        },
+        select: {
+          FinanceData: {
+            select: {
+              transation: true,
+              day: true,
+              hour: true,
+              minutes: true,
+              month: true,
+              year: true,
+              value: true,
+              id: true,
+            },
+          },
+        },
+      });
+      res.status(200).json(allDeposits);
     } catch (error) {
       res.status(400).json(error);
     }
