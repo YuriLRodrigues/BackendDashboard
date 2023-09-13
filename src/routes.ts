3caab40cf2f70be221d40f5218ea126c3d1ebd1c
yurilrodrigues <rodriguesyuri769@gmail.com>
2023-09-13 16:04:19 -0300

    feat: atualizando a chamada e caminho das rotas da api

--- src/routes.ts
+++ src/routes.ts
@@ -13,12 +13,15 @@ const financeController = new FinanceController();
 
 router.get("/access", AuthMiddleware(["adm"]), accessController.getAll);
 router.get("/users", AuthMiddleware(["adm"]), userController.getAll);
-router.get("/finances",  AuthMiddleware(["adm"]), financeController.getAll);
+router.get("/myextracts", AuthMiddleware(["adm", "user"]), financeController.getExtracts);
+router.get("/mybalance", AuthMiddleware(["adm", "user"]), financeController.getBalance);
+router.get("/mytransactions", AuthMiddleware(["adm", "user"]), financeController.getTransations);
 
 router.post("/signin", sessionController.signIn);
 router.post("/newaccess", AuthMiddleware(["adm"]), accessController.create);
 router.post("/user", userController.create);
-router.post("/newbuy", AuthMiddleware(["adm", "user"]), financeController.newBuy);
+router.post("/newexpense", AuthMiddleware(["adm", "user"]), financeController.newExpense); 
+router.post("/newdeposit", AuthMiddleware(["adm", "user"]), financeController.newDeposit); 
 
 
 export { router };
