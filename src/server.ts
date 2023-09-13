0932d9cad7e0c48c970c8d303e062a58233c3463
yurilrodrigues <rodriguesyuri769@gmail.com>
2023-09-13 16:19:58 -0300

    chore: Configurando o cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import { router } from "./routes";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(router)
-dotenv.config()
+app.use(express.json());
+app.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  next();
+});
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+app.use(router);
+dotenv.config();
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Rodando na porta ${port}`))
+app.listen(port, () => console.log(`Rodando na porta ${port}`));
