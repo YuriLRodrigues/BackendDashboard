b2a4479eaa36e299fd636f117cac58fffcbcd395
yurilrodrigues <rodriguesyuri769@gmail.com>
2023-09-17 18:16:37 -0300

    feat: passando o token na rota de signin

--- src/controller/SessionController.ts
+++ src/controller/SessionController.ts
@@ -6,52 +6,69 @@ import { sign } from "jsonwebtoken";
 export class SessionController {
   async signIn(req: Request, res: Response) {
     try {
-      const {email, password} = req.body
-      
+      const { email, password } = req.body;
+
       const user = await prisma.user.findUnique({
         where: {
-          email
+          email,
         },
-        include: {
+        select: {
+          balance: true,
+          name: true,
+          email: true,
+          id: true,
+          password: true,
           userAccess: {
             select: {
               Access: {
                 select: {
-                  name: true
-                }
-              }
-            }
-          }
-        }
-      })
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      });
 
       if (!user) {
-        return res.status(400).json({error: 'User not found'})
+        return res.status(400).json({ error: "User not found" });
       }
 
-      const passwordMatch = await compare(password, user.password)
+      const passwordMatch = await compare(password, user.password);
 
       if (!passwordMatch) {
-        return res.status(400).json({error: 'Invalid password'})
+        return res.status(400).json({ error: "Invalid password" });
       }
 
-      const MY_SECRET_KEY = process.env.SECRET
+      const userWithoutPassword = {
+        name: user.name,
+        email: user.email,
+        id: user.id,
+        balance: user.balance,
+        userAccess: user.userAccess,
+      };
 
-      if (!MY_SECRET_KEY){
-        return res.status(400).json({error: 'Invalid secret key'})
+      const MY_SECRET_KEY = process.env.SECRET;
+
+      if (!MY_SECRET_KEY) {
+        return res.status(400).json({ error: "Invalid secret key" });
       }
 
-      const token = sign({
-        userId: user.id, roles: user.userAccess.map((role: any)=> role.Access?.name)
-      }, MY_SECRET_KEY, {
-        algorithm: "HS256",
-        expiresIn: "24h"
-      })
+      const token = sign(
+        {
+          userId: user.id,
+          roles: user.userAccess.map((role: any) => role.Access?.name),
+        },
+        MY_SECRET_KEY,
+        {
+          algorithm: "HS256",
+          expiresIn: "24h",
+        }
+      );
 
-      res.status(200).json({token})
-
+      res.status(200).json({ ...userWithoutPassword, token });
     } catch (error) {
-      res.status(400).json({error})
+      res.status(400).json({ error });
     }
-}
+  }
 }
